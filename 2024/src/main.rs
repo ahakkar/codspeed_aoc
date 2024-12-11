cc3b2e8f155d037f962af555a9a140627ffe7eec
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-12-11 21:37:15 +0200

    change mod to day11

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs, time::Instant};
-use cs_aoc_2024::day_11;
+use cs_aoc_2024::day11;
 
 pub fn read_data_from_file(file_path: &str) -> String {
     fs::read_to_string(file_path)
@@ -10,11 +10,11 @@ fn main() {
     let input = read_data_from_file("input/2024/day11.txt");
 
     let mut start = Instant::now();
-    let silver = day_11::part1(&input);
+    let silver = day11::part1(&input);
     let silver_duration = start.elapsed();
 
     start = Instant::now();
-    let gold = day_11::part2(&input);
+    let gold = day11::part2(&input);
     let gold_duration = start.elapsed();
 
     println!("Silver: {}, Gold: {}", silver, gold);
